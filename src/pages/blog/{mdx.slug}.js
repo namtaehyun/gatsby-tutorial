2e48afa55dfaf59e251dc2c5384b620f1795bb6c
namtaehyun <taehyunnam8034@gmail.com>
2021-09-10 23:58:42 +0900

    Add post page

---
+++ src/pages/blog/{mdx.slug}.js
@@ -0,0 +1,31 @@
+import * as React from 'react'
+import { graphql } from 'gatsby'
+import { MDXRenderer } from 'gatsby-plugin-mdx'
+import Layout from '../../components/layouts/layout'
+
+const BlogPost = ({ data }) => {
+  const { frontmatter, body } = data.mdx;
+
+  return (
+    <Layout pageTitle={frontmatter.title}>
+      <p>{frontmatter.date}</p>
+      <MDXRenderer>
+        {body}
+      </MDXRenderer>
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  query ($id: String) {
+    mdx(id: {eq: $id}) {
+      frontmatter {
+        title
+        date
+      }
+      body
+    }
+  }
+`
+// highlight-end
+export default BlogPost
