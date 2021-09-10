25bcaae86335bdb9238c64060a44e5063e1c2aec
namtaehyun <taehyunnam8034@gmail.com>
2021-09-10 23:24:01 +0900

    Add package, config

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,13 @@ module.exports = {
   plugins: [
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog/`,
+      }
+    },
+    "gatsby-plugin-mdx",
   ],
 };
